bae73729672ada9d541e9732fd169832ae82545a
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-08-24 18:01:05 +0200

    refactor: fix isssues in current entity build
    by this commit I've changed the base structure of our photo and pin entity

--- VirtualTourist/Photo.swift
+++ VirtualTourist/Photo.swift
@@ -6,4 +6,66 @@
 //  Copyright © 2017 Patrick Paechnatz. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class Photo: NSManagedObject {
+    
+    @NSManaged var photoURL: String
+    @NSManaged var imagePath: String?
+    @NSManaged var pin: Pin
+    
+    override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    init(photoURL: String, pin: Pin, context: NSManagedObjectContext) {
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Photo", in: context)!
+        super.init(entity: entity, insertInto: context)
+        
+        self.photoURL = photoURL
+        self.pin = pin
+    }
+    
+    var image: UIImage? {
+        
+        if imagePath != nil {
+            let fileURL = getFileURL()
+            return UIImage(contentsOfFile: fileURL.path)
+        }
+        
+        return nil
+    }
+    
+    func getFileURL() -> URL {
+        
+        let fileName = (imagePath! as NSString).lastPathComponent
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let pathArray:[String] = [dirPath, fileName]
+        let fileURL = NSURL.fileURL(withPathComponents: pathArray)
+        
+        return fileURL!
+    }
+    
+    override func prepareForDeletion() {
+        
+        if (imagePath == nil) { return }
+        
+        let fileURL = getFileURL()
+        
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            
+            do {
+                
+                try FileManager.default.removeItem(atPath: fileURL.path)
+                
+            } catch let error as NSError {
+                
+                print(error.userInfo)
+                
+            }
+        }
+    }
+}
--- VirtualTourist/Pin.swift
+++ VirtualTourist/Pin.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017 Patrick Paechnatz. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+import CoreData
+
+class Pin: NSManagedObject, MKAnnotation {
+    
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var numPages: NSNumber?
+    @NSManaged var photos:[Photo]
+    
+    var isDownloading = false
+    
+    override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    init(coordinate: CLLocationCoordinate2D, context: NSManagedObjectContext) {
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Pin", in: context)!
+        super.init(entity: entity, insertInto: context)
+        
+        self.latitude = coordinate.latitude
+        self.longitude = coordinate.longitude
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        
+        get { return CLLocationCoordinate2DMake(latitude, longitude) }
+        set {
+            self.latitude = newValue.latitude
+            self.longitude = newValue.longitude
+        }
+    }
+}
