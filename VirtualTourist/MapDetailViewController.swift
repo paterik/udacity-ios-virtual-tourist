3b0ea6d7c0837e53b5964f98978622bede455714
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-08-27 17:12:00 +0200

    feature: add new mapDetailViewController and extend mapViewController
    by this commit I've implemented an extension for my mapViewController and add our new mapDetailViewController class

---
+++ VirtualTourist/MapDetailViewController.swift
@@ -0,0 +1,104 @@
+//
+//  MapDetailViewController.swift
+//  VirtualTourist
+//
+//  Created by Patrick Paechnatz on 27.08.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapDetailViewController: BaseController, MKMapViewDelegate {
+    
+    //
+    // MARK: IBOutlet Variables
+    //
+    
+    @IBOutlet weak var btnBackToMapItem: UIBarButtonItem!
+    @IBOutlet weak var miniMapView: MKMapView!
+    
+    //
+    // MARK: Class Constants
+    //
+    
+    let mapPinIdentifier = "MiniMapPin"
+    let mapPinImageName = "icnMapPin_v1"
+    
+    //
+    // MARK: Class Variables
+    //
+    
+    var pin:Pin!
+ 
+    //
+    // MARK: UIViewController Overrides
+    //
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        mapSetup()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+    }
+    
+    //
+    // MARK: IBAction Methods
+    //
+    
+    @IBAction func btnBackToMapAction(_ sender: Any) {
+    
+        dismiss(animated: true, completion: nil)
+    
+    }
+    
+    //
+    // MARK: MapView Delegates
+    //
+    
+    func mapView(
+       _ mapView: MKMapView,
+         viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView: MKAnnotationView
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: mapPinIdentifier) {
+            
+            dequeuedView.annotation = annotation
+            annotationView = dequeuedView
+            
+        } else {
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: mapPinIdentifier)
+            annotationView.image = UIImage(named: mapPinImageName)
+            annotationView.canShowCallout = false
+            annotationView.isDraggable = false
+        }
+        
+        return annotationView
+    }
+    
+    //
+    // MARK: MapView Helper Methods
+    //
+    
+    func mapSetup() {
+        
+        // @todo (v1.0.5): move this as property pack deep inside the corresponding PIN entity
+        let pinCenter = CLLocationCoordinate2D(latitude: pin.coordinate.latitude, longitude: pin.coordinate.longitude)
+        let pinRegion = MKCoordinateRegion(center: pinCenter, span: MKCoordinateSpan(latitudeDelta: 0.375, longitudeDelta: 0.375))
+
+        miniMapView.delegate = self
+        miniMapView.setRegion(pinRegion, animated: true)
+        miniMapView.addAnnotation(pin)
+        
+    }
+}
---
+++ VirtualTourist/MapViewControllerExtension.swift
@@ -0,0 +1,109 @@
+//
+//  MapViewControllerExtension.swift
+//  VirtualTourist
+//
+//  Created by Patrick Paechnatz on 27.08.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreStore
+
+extension MapViewController {
+
+    func loadMapRegion() {
+        
+        mapViewRegion = CoreStore.fetchOne(From<MapRegion>())
+        if mapViewRegion == nil {
+            
+            _ = try? CoreStore.perform(
+                synchronous: { (transaction) in
+                    
+                    mapViewRegion = transaction.create(Into<MapRegion>())
+                    mapViewRegion?.region = mapView.region
+                }
+            )
+            
+            mapViewRegion = CoreStore.fetchOne(From<MapRegion>())
+        }
+        
+        mapViewRegionObjectId = mapViewRegion!.objectID
+        mapView.region = mapViewRegion!.region
+
+    }
+    
+    func saveMapRegion() {
+        
+        CoreStore.perform(
+            asynchronous: { (transaction) -> MapRegion in
+                self.mapViewRegion = transaction.fetchExisting(self.mapViewRegionObjectId!)!
+                self.mapViewRegion?.region = self.mapView.region
+                
+                return self.mapViewRegion!
+                
+            },  success: { (transactionRegion) in
+            
+                self.mapViewRegion = CoreStore.fetchExisting(transactionRegion)!
+                self.mapViewRegionObjectId = self.mapViewRegion?.objectID // just to be sure ;)
+                if self.appDebugMode == true { print ("--- mapRegionObjID: \(self.mapViewRegionObjectId!) updated ---") }
+            
+            },  failure: { (error) in print (error) }
+        )
+    }
+    
+    func loadMapAnnotations() {
+        
+        if let mapViewPins = _getAllPins() {
+            mapView.addAnnotations(mapViewPins)
+        }
+    }
+    
+    func _getAllPins() -> [Pin]? {
+        
+        return CoreStore.fetchAll(From<Pin>())
+    }
+    
+    func _deletePin (_ targetPin: Pin!)  {
+        
+        CoreStore.perform(
+            asynchronous: { (transaction) -> Void in
+                transaction.deleteAll(
+                    From<Pin>(),
+                    Where("metaHash", isEqualTo: targetPin.metaHash)
+                )
+            },
+            success: { _ in
+                self.mapView.removeAnnotation(targetPin)
+                self._pinSelected = nil
+                if self.appDebugMode == true {
+                    print ("[_DEV_] \(targetPin.coordinate) deleted from persistance layer!")
+                }
+                
+            },
+            failure: { (error) in
+                self._handleErrorAsSimpleDialog("Error", error.localizedDescription)
+                return
+            }
+        )
+    }
+    
+    func _deleteAllPins() {
+        
+        let numOfCurrentPins = CoreStore.fetchAll(From<Pin>())?.count
+        
+        CoreStore.perform(
+            asynchronous: { (transaction) -> Void in
+                transaction.deleteAll(From<Pin>())
+            },
+            completion: { _ in
+                
+                self.mapView.removeAnnotations(self.mapView.annotations)
+                if self.appDebugMode == true {
+                    print ("[_DEV_] all \(numOfCurrentPins!) previously saved pins deleted from persitance layer")
+                }
+            }
+        )
+    }
+}
