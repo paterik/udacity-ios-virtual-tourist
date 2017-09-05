a73bcc5cbf6ff94e4617072015dd750fd0d5e24d
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-09-05 18:28:55 +0200

    refactor: massive refactoring of photo entity object
    by this commit I've updated our photo entity object after bug hunting safari

--- VirtualTourist/Photo.swift
+++ VirtualTourist/Photo.swift
@@ -12,47 +12,9 @@ import MapKit
 
 class Photo: NSManagedObject {
     
-    @NSManaged var photoURL: String
-    @NSManaged var imagePath: String?
+    @NSManaged var imageSourceURL: String
+    @NSManaged var imageRaw: Data?
+    @NSManaged var imagePreview: Data?
+    @NSManaged var imageHash: String
     @NSManaged var pin: Pin
-    
-    var image: UIImage? {
-        
-        if imagePath != nil {
-            let fileURL = getFileURL()
-            return UIImage(contentsOfFile: fileURL.path)
-        }
-        
-        return nil
-    }
-    
-    func getFileURL() -> URL {
-        
-        let fileName = (imagePath! as NSString).lastPathComponent
-        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        let pathArray:[String] = [dirPath, fileName]
-        let fileURL = NSURL.fileURL(withPathComponents: pathArray)
-        
-        return fileURL!
-    }
-    
-    override func prepareForDeletion() {
-        
-        if (imagePath == nil) { return }
-        
-        let fileURL = getFileURL()
-        
-        if FileManager.default.fileExists(atPath: fileURL.path) {
-            
-            do {
-                
-                try FileManager.default.removeItem(atPath: fileURL.path)
-                
-            } catch let error as NSError {
-                
-                print(error.userInfo)
-                
-            }
-        }
-    }
 }
