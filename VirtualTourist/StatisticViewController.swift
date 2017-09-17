7857a981def2c0ef42eb7f925b7c477fccca9f2a
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-09-17 11:01:35 +0200

    feature: add statistic value generator for statisticsView
    by this commit I've implemented the value generator for corresponding statisticsView

--- VirtualTourist/StatisticViewController.swift
+++ VirtualTourist/StatisticViewController.swift
@@ -12,6 +12,80 @@ import CoreStore
 
 class StatisticViewController: BaseController {
 
+    
+    @IBOutlet weak var lblLocationCount: UILabel!
+    @IBOutlet weak var lblPhotosCount: UILabel!
+    @IBOutlet weak var lblPhotoStorageUsed: UILabel!
+    @IBOutlet weak var btnResetLocations: UIButton!
+    
+    //
+    // MARK: UIViewController Overrides
+    //
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        setupUIDataLocationCount()
+        setupUIDataPhotoAndStorageCount()
+    }
+    
+    func setupUIDataPhotoAndStorageCount() {
+
+        var sizeImageOriginKB: Double = 0.0
+        var sizeImageSumKB: Double = 0.0
+    
+        CoreStore.perform(
+            
+            asynchronous: { (transaction) -> [Photo]? in
+                
+                return transaction.fetchAll(From<Photo>())
+            },
+            
+            success: { (transactionPhotos) in
+                
+                for _photo in transactionPhotos! {
+                    
+                    if  let _imageOrigin = _photo.imageRaw {
+                        sizeImageOriginKB = Double(_imageOrigin.count) / 1024.0 / 1024.0
+                        sizeImageSumKB += sizeImageOriginKB
+                    }
+                }
+                
+                if transactionPhotos?.isEmpty == false {
+                    self.lblPhotosCount.text = NSString(format: "%d", transactionPhotos!.count) as String
+                    self.lblPhotoStorageUsed.text = NSString(format: "%.02f MB", sizeImageSumKB) as String
+                }
+            },
+            
+            failure: { (error) in }
+        )
+    }
+    
+    func setupUIDataLocationCount() {
+        
+        CoreStore.perform(
+            
+            asynchronous: { (transaction) -> [Pin]? in
+                
+                return transaction.fetchAll(From<Pin>())
+            },
+            
+            success: { (transactionPins) in
+                
+                if transactionPins?.isEmpty == false {
+                    self.lblLocationCount.text = NSString(format: "%d", transactionPins!.count) as String
+                }
+            },
+            
+            failure: { (error) in }
+        )
+    }
+    
+    @IBAction func btnResetLocationsAction(_ sender: Any) {
+    
+    }
+    
     @IBAction func btnReturnToMapAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
